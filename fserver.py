b5775e11464e05ad15a189603c486a45e432b591
Rory Gianni <giannichan@gmail.com>
2016-03-25 19:48:01 +0000

    added example of electrum wrapper

--- fserver.py
+++ fserver.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from subprocess import Popen, PIPE
 
 app = Flask(__name__)
 
@@ -8,6 +9,13 @@ app = Flask(__name__)
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+@app.route("/test")
+def testing():
+    p = Popen(['electrum', 'getaddresshistory', '1AR9FJLYUb9cqojiuTwrD7awP18FfXJkoQ'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    output, err = p.communicate()
+    rc = p.returncode
+    return output
 
 if __name__ == "__main__":
     app.run()
