1d617aebf4568384eb7432211afbae72256267b7
Rory Gianni <giannichan@gmail.com>
2016-05-19 17:08:08 +0100

    read all inputs

--- mcp3008/read_board.py
+++ mcp3008/read_board.py
@@ -9,38 +9,38 @@ DEBUG = 0
 
 # read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
 def readadc(adcnum, clockpin, mosipin, misopin, cspin):
-        if ((adcnum > 7) or (adcnum < 0)):
-                return -1
-        GPIO.output(cspin, True)
+  if ((adcnum > 7) or (adcnum < 0)):
+          return -1
+  GPIO.output(cspin, True)
 
-        GPIO.output(clockpin, False)  # start clock low
-        GPIO.output(cspin, False)     # bring CS low
+  GPIO.output(clockpin, False)  # start clock low
+  GPIO.output(cspin, False)     # bring CS low
 
-        commandout = adcnum
-        commandout |= 0x18  # start bit + single-ended bit
-        commandout <<= 3    # we only need to send 5 bits here
-        for i in range(5):
-                if (commandout & 0x80):
-                        GPIO.output(mosipin, True)
-                else:
-                        GPIO.output(mosipin, False)
-                commandout <<= 1
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
+  commandout = adcnum
+  commandout |= 0x18  # start bit + single-ended bit
+  commandout <<= 3    # we only need to send 5 bits here
+  for i in range(5):
+          if (commandout & 0x80):
+                  GPIO.output(mosipin, True)
+          else:
+                  GPIO.output(mosipin, False)
+          commandout <<= 1
+          GPIO.output(clockpin, True)
+          GPIO.output(clockpin, False)
 
-        adcout = 0
-        # read in one empty bit, one null bit and 10 ADC bits
-        for i in range(12):
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
-                adcout <<= 1
-                if (GPIO.input(misopin)):
-                        adcout |= 0x1
+  adcout = 0
+  # read in one empty bit, one null bit and 10 ADC bits
+  for i in range(12):
+          GPIO.output(clockpin, True)
+          GPIO.output(clockpin, False)
+          adcout <<= 1
+          if (GPIO.input(misopin)):
+                  adcout |= 0x1
 
-        GPIO.output(cspin, True)
-        
-        adcout >>= 1       # first bit is 'null' so drop it
-        return adcout
+  GPIO.output(cspin, True)
+  
+  adcout >>= 1       # first bit is 'null' so drop it
+  return adcout
 
 # change these as desired - they're the pins connected from the
 # SPI port on the ADC to the Cobbler
@@ -56,47 +56,36 @@ GPIO.setup(SPICLK, GPIO.OUT)
 GPIO.setup(SPICS, GPIO.OUT)
 
 #MCP Channel to Control Mappings
-serbatoio = 0   #pin 10
-al_fondi = 1    #pin 12
-al_generico = 2 #pin 14
-al_calcif = 3   #pin 16
-premacinato = 4 #pin 18
-tazza1 = 5      #pin 21
-tazza2 = 6      #pin 23
-vapore = 7      #pin 25
+# serbatoio   #pin 10
+# al_fondi    #pin 12
+# al_generico #pin 14
+# al_calcif   #pin 16
+# premacinato #pin 18
+# tazza1      #pin 21
+# tazza2      #pin 23
+# vapore      #pin 25
 
-serbatoio_readings = []
-tazza1_readings    = []
-al_fondi_readings  = []
-al_fondi_average   = 0
-serbatoio_average  = 0
-tazza1_average     = 0
+channel_names = ["serbatoio","al_fondi","al_generico","al_calcif","premacinato","tazza1","tazza2","vapore"]
+channel_readings   = [[],[],[],[],[],[],[],[]]
+channel_averages   = []
 
 while True:
-        serbatoio_value = readadc(serbatoio, SPICLK, SPIMOSI, SPIMISO, SPICS) # read the analog pin
-        serbatoio_value = round(serbatoio_value / 10.24, 2) # convert 10bit adc0 (0-1024) trim pot read into 0-100 value
-        serbatoio_readings.append(serbatoio_value)
 
-        tazza1_value = readadc(tazza1, SPICLK, SPIMOSI, SPIMISO, SPICS)
-        tazza1_value = round(tazza1_value / 10.24, 2)
-        tazza1_readings.append(tazza1_value)
+  channel = 0
+  while channel <= 7:
+    channel_value = readadc(channel, SPICLK, SPIMOSI, SPIMISO, SPICS) # read the analog pin
+    channel_value = round(channel_value / 10.24, 2) # convert 10bit adc0 (0-1024) trim pot read into 0-100 value
+    channel_readings[channel].append(channel_value)
+    channel+=1
 
-        al_fondi_value = readadc(al_fondi, SPICLK, SPIMOSI, SPIMISO, SPICS)
-        al_fondi_value = round(al_fondi_value / 10.24, 2)
-        al_fondi_readings.append(al_fondi_value)
-        
-        if len(serbatoio_readings) >= 100:
-                serbatoio_average  = sum(serbatoio_readings)/len(serbatoio_readings)
-                #tazza1_average     = sum(tazza1_readings)/len(tazza1_readings)
-                al_fondi_average     = sum(al_fondi_readings)/len(al_fondi_readings)
-                
-                print time.time(),
-                print ", ", serbatoio_average,
-                print ", ", al_fondi_average
-
-                serbatoio_readings = []
-                #tazza1_readings   = []
-                al_fondi_readings  = []
-
-        # hang out and do nothing for a half second
-        time.sleep(0.0005)
+  if len(channel_readings[0]) >= 150:
+    print time.time(),
+    channel = 0
+    while channel <= 7:
+      channel_average = sum(channel_readings[channel])/len(channel_readings[channel])
+      print ", ", channel_names[channel], 
+      print ": ", channel_average,
+      if channel==7
+        print ""
+  # hang out and do nothing for a half second
+  time.sleep(0.0005)
