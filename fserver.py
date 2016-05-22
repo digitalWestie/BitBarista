d1271f9807ed96d87ee43ae67bfac87c5ecb9e26
Rory Gianni <giannichan@gmail.com>
2016-05-22 22:11:22 +0100

    add reader example and js block to templates

--- fserver.py
+++ fserver.py
@@ -4,27 +4,35 @@ from flask import send_from_directory
 from flask import request
 from subprocess import Popen, PIPE
 
+import fake_board_reader #board_reader on pi
+
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return render_template('root.html')
+  state = fake_board_reader.read_state()
+  return render_template('root.html', state=state)
 
 @app.route('/assets/<path:path>')
 def send_asset(path):
-    return send_from_directory('assets', path)
+  return send_from_directory('assets', path)
+
+@app.route('/state/')
+def get_state():
+  state = fake_board_reader.read_state()
+  return 'x'
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+  return render_template('hello.html', name=name)
 
 @app.route("/test")
 def testing():
-    p = Popen(['electrum', 'getaddresshistory', '1AR9FJLYUb9cqojiuTwrD7awP18FfXJkoQ'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    output, err = p.communicate()
-    rc = p.returncode
-    return output
+  p = Popen(['electrum', 'getaddresshistory', '1AR9FJLYUb9cqojiuTwrD7awP18FfXJkoQ'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+  output, err = p.communicate()
+  rc = p.returncode
+  return output
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
