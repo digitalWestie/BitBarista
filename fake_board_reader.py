49cc9603bc3fe936a31afef58e0be28434aa17d2
Rory Gianni <giannichan@gmail.com>
2016-05-27 17:44:07 +0100

    adds randomised state generation

--- fake_board_reader.py
+++ fake_board_reader.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 import time
+from random import randint
 
 def read_state():
   state = {"overall":"","serbatoio":"", "al_fondi":"", 
     "al_generico":"", "al_calcif":"", "premacinato":"", 
     "tazza1":"","tazza2":"","vapore":""}
+  
+  if randint(0,9) > 2:
+    state["overall"] = 'ready'
+  else:
+    state["overall"] = ''
 
-  state["overall"] = 'ready'
   return state
