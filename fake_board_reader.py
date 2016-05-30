03d2a9bb98db58b4cf6e3e76fe230ec66dc9bf03
Rory Gianni <giannichan@gmail.com>
2016-05-30 17:30:40 +0100

    adds status messages

--- fake_board_reader.py
+++ fake_board_reader.py
@@ -3,13 +3,14 @@ import time
 from random import randint
 
 def read_state():
-  state = {"overall":"","serbatoio":"", "al_fondi":"", 
+  state = {"overall":"", "message":"", "serbatoio":"", "al_fondi":"", 
     "al_generico":"", "al_calcif":"", "premacinato":"", 
     "tazza1":"","tazza2":"","vapore":""}
   
-  if randint(0,9) > 2:
+  if randint(0,9) > 4:
     state["overall"] = 'ready'
   else:
     state["overall"] = ''
+    state["message"] = "Water is missing, could you replace it?"
 
   return state
