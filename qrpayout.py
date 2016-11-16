f66826083696bb10f4037f56ee415084207a998b
Roryg <giannichan@gmail.com>
2016-11-16 15:53:03 +0000

    adds script to extract btc url qr data

---
+++ qrpayout.py
@@ -0,0 +1,43 @@
+from xml.dom.minidom import parse, parseString
+from subprocess import Popen, PIPE
+
+def read_image():
+  p = Popen(['zbarimg', '-q', '--xml', 'image.jpg'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+  output, err = p.communicate()
+  if p.returncode == 0:
+    dom = parseString(output)
+    nodes = dom.getElementsByTagName('data')
+    if nodes.length == 0:
+      return False
+    return nodes[0].firstChild.data
+  else:
+    return False
+
+def parse_url(url): 
+  #separate address & params
+  result = { 'params': {} }
+  split = url.split('?')
+  #extract address
+  address_half = split[0]
+  address_split = address_half.split('bitcoin:')
+  if len(address_split) == 2:
+    result['address'] = address_split[1]
+  else:
+    return False
+  #extract params
+  if len(split) > 1:
+    params_half = split[1]
+    params = params_half.split('&')
+    for param in params:
+      print param
+      pair = param.split('=')
+      result['params'][pair[0]] = pair[1] 
+  return result
+
+result = read_image()
+if (result):
+  print parse_url(result)
+else:
+  print "Failed"
+
+
