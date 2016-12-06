1183af330e4edbc9d0f67a7c068c3503bcc51bfe
Roryg <giannichan@gmail.com>
2016-12-06 17:55:00 +0000

    range test

--- servid.py
+++ servid.py
@@ -9,10 +9,9 @@ stream = io.BytesIO()
 for i in range(10):
   with picamera.PiCamera() as camera:
     #camera.start_preview()
-    time.sleep(0.1)
+    time.sleep(0.5)
     camera.capture(stream, format='jpeg')
   # "Rewind" the stream to the beginning so we can read its content
   stream.seek(0)
   image = Image.open(stream)
-  image.save("your_file.jpeg")
-
+  image.save('image%02d.jpg' % i)
