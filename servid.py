8ef95ce0c064aa0ec4aea741cb78ad35407c9fb7
Roryg <giannichan@gmail.com>
2016-12-06 17:49:05 +0000

    experiment

---
+++ servid.py
@@ -0,0 +1,15 @@
+import io
+import time
+import picamera
+from PIL import Image
+
+# Create the in-memory stream
+stream = io.BytesIO()
+with picamera.PiCamera() as camera:
+  #camera.start_preview()
+  time.sleep(0.1)
+  camera.capture(stream, format='jpeg')
+# "Rewind" the stream to the beginning so we can read its content
+stream.seek(0)
+image = Image.open(stream)
+image.save("your_file.jpeg")
