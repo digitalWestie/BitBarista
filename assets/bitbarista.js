15a68d81cd2c0de145034d380b4b02602e4594a2
RoryG <giannichan@gmail.com>
2017-01-12 12:23:24 +0000

    two space indent

--- assets/bitbarista.js
+++ assets/bitbarista.js
@@ -1,5 +1,5 @@
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 var stateCallback = function(data) {
